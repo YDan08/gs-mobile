5d22438c2c29330b7defe1aed2c945bf6b83b7a7
YDan08 <dasyamashita@gmail.com>
2022-11-06 15:19:02 -0300

    feat: criar projeot react-native

---
+++ src/routes/Navigation.js
@@ -0,0 +1,11 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { Home } from "../screens/Home"
+const { Screen, Navigator } = createNativeStackNavigator()
+
+export const Navigation = () => {
+  return (
+    <Navigator>
+      <Screen name='home' component={Home} options={{ headerShown: false }} />
+    </Navigator>
+  )
+}
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { Navigation } from "./Navigation"
+
+export const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
+  )
+}
+
+export default Routes

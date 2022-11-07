f0fc83f474d1a8c7fd1a4701614e88ab5659b43e
YDan08 <dasyamashita@gmail.com>
2022-11-07 17:23:46 -0300

    feat: criar tela de home

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,13 +1,78 @@
 import React from "react"
-import { View } from "react-native"
-import { Text } from "react-native-paper"
+import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native"
+import { Button, Text } from "react-native-paper"
 
-export const Home = () => {
+export const Home = ({ navigation }) => {
   return (
-    <View>
-      <Text>alo</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text variant='headlineSmall' style={{ marginTop: 20 }}>
+        Lista de ocorrências
+      </Text>
+
+      <ScrollView style={styles.containerOcorrencias}>
+        <View style={styles.divOcorrencia}>
+          <Text>Rua: Rua Augusto</Text>
+          <Text>Bairro: Vila Lara</Text>
+          <Text>Nível de perigo: Alto</Text>
+        </View>
+        <View style={styles.divOcorrencia}>
+          <Text>Rua: Rua Augusto</Text>
+          <Text>Bairro: Vila Lara</Text>
+          <Text>Nível de perigo: Alto</Text>
+        </View>
+        <View style={styles.divOcorrencia}>
+          <Text>Rua: Rua Augusto</Text>
+          <Text>Bairro: Vila Lara</Text>
+          <Text>Nível de perigo: Alto</Text>
+        </View>
+        <View style={styles.divOcorrencia}>
+          <Text>Rua: Rua Augusto</Text>
+          <Text>Bairro: Vila Lara</Text>
+          <Text>Nível de perigo: Alto</Text>
+        </View>
+        <View style={styles.divOcorrencia}>
+          <Text>Rua: Rua Augusto</Text>
+          <Text>Bairro: Vila Lara</Text>
+          <Text>Nível de perigo: Alto</Text>
+        </View>
+        <View style={styles.divOcorrencia}>
+          <Text>Rua: Rua Augusto</Text>
+          <Text>Bairro: Vila Lara</Text>
+          <Text>Nível de perigo: Alto</Text>
+        </View>
+      </ScrollView>
+
+      <Button
+        mode='contained'
+        style={styles.botao}
+        onPress={() => navigation.navigate("cadastroOcorrencia")}
+      >
+        Cadastar nova ocorrência
+      </Button>
+    </SafeAreaView>
   )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 20,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  divOcorrencia: {
+    width: 320,
+    padding: 20,
+    borderRadius: 12,
+    borderWidth: 1,
+    marginVertical: 12,
+  },
+  containerOcorrencias: {
+    marginVertical: 20,
+  },
+  botao: {
+    width: 300,
+  },
+})

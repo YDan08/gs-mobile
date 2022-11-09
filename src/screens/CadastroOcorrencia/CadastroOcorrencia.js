b2ff9ff24e5da9a82741100fa2ea6daab2acb335
YDan08 <dasyamashita@gmail.com>
2022-11-09 13:57:18 -0300

    feat: adicionar inputDate no cadastro ocorrencia

--- src/screens/CadastroOcorrencia/CadastroOcorrencia.js
+++ src/screens/CadastroOcorrencia/CadastroOcorrencia.js
@@ -1,78 +1,102 @@
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker"
+import moment from "moment/moment"
+import { useState } from "react"
 import { Controller, useForm } from "react-hook-form"
-import { SafeAreaView, StyleSheet, View } from "react-native"
+import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native"
 import { Button, Text, TextInput } from "react-native-paper"
 
 export const CadastroOcorrencia = ({ navigation }) => {
   const { handleSubmit, control } = useForm()
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text variant='headlineMedium'>Cadastro de ocorrência</Text>
-      <View style={styles.divInputs}>
-        <Controller
-          name='rua'
-          control={control}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              label='Rua'
-              mode='outlined'
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-            />
-          )}
-        />
-        <Controller
-          name='bairro'
-          control={control}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              label='Bairro'
-              mode='outlined'
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-            />
-          )}
-        />
-        <Controller
-          name='cidade'
-          control={control}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              label='Cidade'
-              mode='outlined'
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-            />
-          )}
-        />
+  const [date, setDate] = useState(new Date())
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate
+    setDate(currentDate)
+  }
 
-        <Controller
-          name='incidente'
-          control={control}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              label='Descrição do incidente'
-              mode='outlined'
-              multiline={true}
-              numberOfLines={10}
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-            />
-          )}
-        />
+  const show = () => {
+    DateTimePickerAndroid.open({
+      value: date,
+      onChange,
+      mode: "date",
+      maximumDate: Date.now(),
+    })
+  }
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Text variant='headlineMedium'>Cadastro de ocorrência</Text>
+        <View style={styles.divInputs}>
+          <Controller
+            name='rua'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                label='Rua'
+                mode='outlined'
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+              />
+            )}
+          />
+          <Controller
+            name='bairro'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                label='Bairro'
+                mode='outlined'
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+              />
+            )}
+          />
+          <Controller
+            name='cidade'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                label='Cidade'
+                mode='outlined'
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+              />
+            )}
+          />
+
+          <Controller
+            name='incidente'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                label='Descrição do incidente'
+                mode='outlined'
+                multiline={true}
+                numberOfLines={10}
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+              />
+            )}
+          />
+          <View style={styles.divDataOcorrencia}>
+            <Text>Data do ocorrido</Text>
+            <Text style={styles.textData}>{moment(date).format("DD/MM/YYYY")}</Text>
+            <Button onPress={show}>mudar</Button>
+          </View>
+        </View>
+        <View style={styles.divBotoes}>
+          <Button mode='contained' onPress={() => navigation.goBack()} style={styles.botao}>
+            Voltar
+          </Button>
+          <Button mode='contained' style={styles.botao} onPress={() => navigation.navigate("home")}>
+            Cadastrar
+          </Button>
+        </View>
       </View>
-      <View style={styles.divBotoes}>
-        <Button mode='contained' onPress={() => navigation.goBack()} style={styles.botao}>
-          Voltar
-        </Button>
-        <Button mode='contained' style={styles.botao} onPress={() => navigation.navigate("home")}>
-          Cadastrar
-        </Button>
-      </View>
-    </SafeAreaView>
+    </ScrollView>
   )
 }
 
@@ -83,7 +107,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    margin: 8,
+    marginVertical: 20,
   },
   divInputs: {
     width: 320,
@@ -99,6 +123,11 @@ const styles = StyleSheet.create({
     width: 300,
     flexDirection: "row",
     justifyContent: "space-between",
-    marginTop: 12,
+  },
+  divDataOcorrencia: {
+    marginVertical: 12,
+  },
+  textData: {
+    marginVertical: 4,
   },
 })
